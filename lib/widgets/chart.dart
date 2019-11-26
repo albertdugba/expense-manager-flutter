1afb53fd301802880bcd89c4a5d4926c33fa2b3b
Albert Dugba <albert.dugba@gmail.com>
2019-11-26 16:06:55 +0000

    added data to charts

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,8 @@
-import 'package:academind_flutter/model/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+import 'package:academind_flutter/model/transaction.dart';
+import 'package:academind_flutter/widgets/chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -10,6 +12,7 @@ class Chart extends StatelessWidget {
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
+      print(weekDay);
       double totalSum = 0.0;
 
       for (var i = 0; i < recentTransactions.length; i++) {
@@ -22,7 +25,16 @@ class Chart extends StatelessWidget {
 
       print(DateFormat.E().format(weekDay));
       print(totalSum);
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
@@ -33,7 +45,14 @@ class Chart extends StatelessWidget {
         elevation: 6.0,
         margin: EdgeInsets.all(20.0),
         child: Row(
-          children: <Widget>[],
-        ));
+            children: groupedTransactionValues.map((data) {
+          return ChartBar(
+            data['day'],
+            data['amount'],
+            totalSpending == 0.0
+                ? 0.0
+                : (data['amount'] as double) / totalSpending,
+          );
+        }).toList()));
   }
 }
